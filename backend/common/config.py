70d4555b7a257c1185d2932cc427a5dc583ced28
Yan Rui <54534120+y4nrui@users.noreply.github.com>
2022-09-11 15:54:50 +0800

    Update config.py

--- backend/common/config.py
+++ backend/common/config.py
@@ -2,3 +2,5 @@ class Config(object):
     def __init__(self):
         self.mongo_uri = "mongodb://localhost:27017/abc_books"
         self.port = "5010"
+        self.secret_key = "secret_key"
+        self.token_expire_minutes = 5
